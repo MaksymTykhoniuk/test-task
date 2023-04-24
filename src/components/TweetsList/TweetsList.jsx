29556bae367ef1ea791457734e187908cd8303a9
MaksymTykhoniuk <mxtkhnk@gmail.com>
2023-04-24 13:57:20 +0300

    commit

--- src/components/TweetsList/TweetsList.jsx
+++ src/components/TweetsList/TweetsList.jsx
@@ -3,15 +3,37 @@ import { BackLink } from '../BackLink/BackLink';
 import { useLocation } from 'react-router-dom';
 import { Container, TweetsUsersList } from './TweetsList.styled';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
-import { fetchTweets, togglefollowUser } from 'services/fethUsers';
+import {
+  fetchTweets,
+  togglefollowUser,
+  fetchfilteredTweets,
+} from 'services/fethUsers';
+import Select from 'react-select';
+
+const options = [
+  { value: 'all', label: 'All' },
+  { value: 'follow', label: 'Follow' },
+  { value: 'following', label: 'Following' },
+];
 
 export const TweetsList = () => {
+  const [selectedOption, setSelectedOption] = useState('all');
   const [page, setPage] = useState(1);
   const queryClient = useQueryClient();
   const location = useLocation();
   const backLinkHref = location.state?.from ?? '/home';
+
+  useEffect(() => {
+    if (selectedOption.value === 'all') {
+      setPage(1);
+    } else if (selectedOption.value === 'follow') {
+      setPage(1);
+    } else if (selectedOption.value === 'following') {
+      setPage(1);
+    }
+  }, [selectedOption.value]);
 
   const updateTweetsMutation = useMutation(item => togglefollowUser(item), {
     onSuccess: () => {
@@ -20,17 +42,20 @@ export const TweetsList = () => {
   });
 
   const { data, isLoading, isError } = useQuery(
-    ['users', page],
-    () => fetchTweets(page),
+    ['users', page, selectedOption],
+    () => fetchTweets(page, selectedOption),
+    { keepPreviousData: true }
+  );
+
+  const { data: filter } = useQuery(
+    ['users', selectedOption],
+    () => fetchfilteredTweets(selectedOption),
     { keepPreviousData: true }
   );
 
   const handleLoadNextBtn = () => setPage(prev => prev + 1);
   const handleLoadPrevBtn = () => setPage(prev => prev - 1);
-
-  const handleFollowUser = item => {
-    updateTweetsMutation.mutate(item);
-  };
+  const handleFollowUser = item => updateTweetsMutation.mutate(item);
 
   if (isLoading) {
     return <p>loading page......</p>;
@@ -43,28 +68,36 @@ export const TweetsList = () => {
   return (
     <Container>
       <BackLink to={backLinkHref}>Back home</BackLink>
-
+      <div>
+        <Select
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+        />
+      </div>
       <TweetsUsersList>
         {data.map(item => (
           <Tweet key={item.id} item={item} followUser={handleFollowUser} />
         ))}
       </TweetsUsersList>
-      <ButtonWrapper>
-        <Button
-          disabled={isLoading || page === 1}
-          onClick={handleLoadPrevBtn}
-          type="button"
-        >
-          load previous
-        </Button>
-        <Button
-          disabled={isLoading || page === 8}
-          onClick={handleLoadNextBtn}
-          type="button"
-        >
-          load next
-        </Button>
-      </ButtonWrapper>
+      {filter.length > 3 && (
+        <ButtonWrapper>
+          <Button
+            disabled={isLoading || page === 1}
+            onClick={handleLoadPrevBtn}
+            type="button"
+          >
+            load previous
+          </Button>
+          <Button
+            disabled={isLoading || page === filter.length / 3}
+            onClick={handleLoadNextBtn}
+            type="button"
+          >
+            load next
+          </Button>
+        </ButtonWrapper>
+      )}
     </Container>
   );
 };
--- src/services/fethUsers.js
+++ src/services/fethUsers.js
@@ -1,23 +1,59 @@
 import axios from 'axios';
 
-export const fetchTweets = async page => {
-  const { data } = await axios.get(
-    `https://64302a62c26d69edc88c56e4.mockapi.io/api/v1/users?page=${page}&limit=3`
-  );
+export const fetchTweets = async (page, filter) => {
+  let queries;
+  if (filter.value === 'all') {
+    queries = '';
+  } else if (filter.value === 'follow') {
+    queries = `isFollowed=${false}`;
+  } else if (filter.value === 'following') {
+    queries = `isFollowed=${true}`;
+  }
 
-  return data;
+  try {
+    const { data } = await axios.get(
+      `https://64302a62c26d69edc88c56e4.mockapi.io/api/v1/users?${queries}&page=${page}&limit=3`
+    );
+
+    return data;
+  } catch (error) {}
+};
+
+export const fetchfilteredTweets = async filter => {
+  let queries;
+  if (filter.value === 'all') {
+    queries = '';
+  } else if (filter.value === 'follow') {
+    queries = `isFollowed=${false}`;
+  } else if (filter.value === 'following') {
+    queries = `isFollowed=${true}`;
+  }
+
+  try {
+    const { data } = await axios.get(
+      `https://64302a62c26d69edc88c56e4.mockapi.io/api/v1/users?${queries}`
+    );
+
+    return data;
+  } catch (error) {
+    alert(error);
+  }
 };
 
 export const togglefollowUser = async user => {
-  const { data } = await axios.put(
-    `https://64302a62c26d69edc88c56e4.mockapi.io/api/v1/users/${user.id}`,
-    {
-      ...user,
-      isFollowed: !user.isFollowed,
-      followers: user.isFollowed
-        ? (user.followers -= 1)
-        : (user.followers += 1),
-    }
-  );
-  return data;
+  try {
+    const { data } = await axios.put(
+      `https://64302a62c26d69edc88c56e4.mockapi.io/api/v1/users/${user.id}`,
+      {
+        ...user,
+        isFollowed: !user.isFollowed,
+        followers: user.isFollowed
+          ? (user.followers -= 1)
+          : (user.followers += 1),
+      }
+    );
+    return data;
+  } catch (error) {
+    alert(error);
+  }
 };
